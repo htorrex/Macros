84de4be628e1b0b18a9502b5574e3e96fa297076
Hector Torres <Hector.Torres@disney.com>
2017-07-14 12:00:36 -0700

    first files

---
+++ src/app_js/app.js
@@ -0,0 +1,7 @@
+var APP_ID = 'D0F15760-B620-6157-FFCA-9D698E68A000';
+var API_KEY = '9CCA6791-5E01-A610-FF5D-A94CC5AF8200';
+var MACROS_TABLE = 'MacrosTable';
+
+Backendless.serverURL = 'https://api.backendless.com';
+Backendless.initApp(APP_ID, API_KEY);
+                    
